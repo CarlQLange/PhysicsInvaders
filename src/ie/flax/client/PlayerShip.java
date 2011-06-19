0885522cb15acef4c8d912887055c0d0604e0b39
Carl Lange <carl@flax.ie>
2011-06-19 19:12:20 +0100

    Few things: keyhandling is now done on the rootpanel, not on the canvas. The PlayerShip is a kinematic body now. Keypresses now change the velocity of the PlayerShip instead of the position.

--- src/ie/flax/client/PlayerShip.java
+++ src/ie/flax/client/PlayerShip.java
@@ -5,44 +5,46 @@ import org.jbox2d.dynamics.BodyType;
 
 import com.google.gwt.event.dom.client.KeyPressEvent;
 import com.google.gwt.event.dom.client.KeyPressHandler;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.user.client.ui.RootPanel;
 
 /*
  * TODO At some point you're gonna wanna change this from directly inheriting
- * PhysicsObject However, this works for the moment. On top of that, adding
+ * PhysicsObject. However, this works for the moment. On top of that, adding
  * another like 70 pixel blocks? Probably not great from a performance point of
  * view.
  */
 public class PlayerShip extends PhysicsObject {
     public PlayerShip(float x, float y, int w, int h) {
-        super(x, y, w, h, BodyType.STATIC);
-
-        PhysicsInvaders.canvas.addKeyPressHandler(new KeyPressHandler() {
+        super(x, y, w, h, BodyType.KINEMATIC);
+        RootPanel.get().addDomHandler(new KeyPressHandler() {
 
             @Override
             public void onKeyPress(KeyPressEvent event) {
-                // FIXME This is not the right way to move this body
-                // better would be giving it velocity or summat
-                // this will do for now.
-                float distanceToMoveInPixels = 3.0f;
+                /*
+                 * FIXME: Not the correct way to move this body. Do it by
+                 * applying force instead.
+                 */
                 if (event.getCharCode() == 'd') {
-                    body.setTransform(
-                            pos.sub(new Vec2(distanceToMoveInPixels
-                                    / -PhysicsInvaders.PTM_RATIO, 0)), 0);
+                    body.setLinearVelocity(new Vec2(3, 0));
                 } else if (event.getCharCode() == 'a') {
-                    body.setTransform(
-                            pos.sub(new Vec2(distanceToMoveInPixels
-                                    / PhysicsInvaders.PTM_RATIO, 0)), 0);
+                    body.setLinearVelocity(new Vec2(-3, 0));
                 } else if (event.getCharCode() == 'w') {
-                    body.setTransform(
-                            pos.sub(new Vec2(0, distanceToMoveInPixels
-                                    / PhysicsInvaders.PTM_RATIO)), 0);
+                    body.setLinearVelocity(new Vec2(0, -3));
                 } else if (event.getCharCode() == 's') {
-                    body.setTransform(
-                            pos.sub(new Vec2(0, distanceToMoveInPixels
-                                    / -PhysicsInvaders.PTM_RATIO)), 0);
+                    body.setLinearVelocity(new Vec2(0, 3));
                 }
             }
-        });
+        }, KeyPressEvent.getType());
+
+        RootPanel.get().addDomHandler(new KeyUpHandler() {
+
+            @Override
+            public void onKeyUp(KeyUpEvent event) {
+                body.setLinearVelocity(new Vec2(0, 0));
+            }
+        }, KeyUpEvent.getType());
 
         this.body.m_fixtureList.m_filter.groupIndex = 1;
     }
